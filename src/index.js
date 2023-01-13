4c789528c693e51ee857f91a8030f11650c9b784
Guilherme Ferreira <guilhermersrferreira@gmail.com>
2023-01-13 16:48:56 +0000

    working through last iteration

--- src/index.js
+++ src/index.js
@@ -22,30 +22,56 @@ const cards = [
   { name: 'spiderman', img: 'spiderman.jpg' },
   { name: 'superman', img: 'superman.jpg' },
   { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
-];
+  { name: 'thor', img: 'thor.jpg' },
+]
 
-const memoryGame = new MemoryGame(cards);
+const memoryGame = new MemoryGame(cards)
 
 window.addEventListener('load', (event) => {
-  let html = '';
+  let html = ''
   memoryGame.cards.forEach((pic) => {
-    html += `
-      <div class="card" data-card-name="${pic.name}">
+    html += `<div class="card" id="${pic.name}">
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
-      </div>
-    `;
-  });
+      </div>`
+  })
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
+  document.querySelector('#memory-board').innerHTML = html
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
-});
+      let isPair = false
+      let isTurned = false
+
+      if (isTurned) {
+        card.classList.remove('turned')
+        isTurned=false
+        console.log(card)
+      } else{
+        card.classList.add('turned')
+        isTurned=true
+        console.log(card)
+
+      }
+        
+      memoryGame.pickedCards.push(card.id)
+      let currentSuperIndex = memoryGame.pickedCards.length - 1
+      let previousSuperIndex = memoryGame.pickedCards.length - 2
+      if (memoryGame.pickedCards.length % 2 === 0) {
+        isPair = memoryGame.checkIfPair(
+          memoryGame.pickedCards[currentSuperIndex],
+          memoryGame.pickedCards[previousSuperIndex]
+        )
+        if (isPair) {
+          memoryGame.pairsClicked++
+          if (memoryGame.checkIfFinished()) {
+            alert('you won')
+          }
+        }
+      }
+    })
+  })
+})
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,43 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
+    this.cards = cards
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
+    let i = this.cards.length
+
+    while (--i > 0) {
+      let randIndex = Math.floor(Math.random() * (i + 1))
+      ;[this.cards[randIndex], this.cards[i]] = [
+        this.cards[i],
+        this.cards[randIndex],
+      ]
+    }
+    return this.cards
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++
+    if(card1 === card2){
+      
+      this.pairsGuessed++
+    } 
+    return card1 === card2
   }
 
   checkIfFinished() {
     // ... write your code here
+
+    return this.pairsGuessed === this.cards.length /2 
+    
   }
 }
